a6d1fc617de2da215ef982833778ca3e0a43b152
Takuya Matsuyama <hi@craftz.dog>
2020-05-06 12:12:48 +0900

    fix(plugin): unload components and mode for math on deactivation

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ import { markdownRenderer } from 'inkdrop'
 import CodeMirror from 'codemirror'
 
 const TeX = require('@matejmazur/react-katex')
+const MATH_MODE_INFO = {
+  name: 'math',
+  mime: 'text/x-latex',
+  mode: 'stex',
+  ext: [],
+  alias: ['inline_math']
+}
 
 class Math extends React.Component {
   static propTypes = {
@@ -48,15 +55,22 @@ module.exports = {
       markdownRenderer.remarkCodeComponents.inline_math = Math
     }
     if (CodeMirror) {
-      CodeMirror.modeInfo.push({
-        name: 'math',
-        mime: 'text/x-latex',
-        mode: 'stex',
-        ext: [],
-        alias: ['inline_math']
-      })
+      CodeMirror.modeInfo.push(MATH_MODE_INFO)
     }
   },
 
-  deactivate() {}
+  deactivate() {
+    if (markdownRenderer) {
+      const { remarkPlugins, remarkCodeComponents } = markdownRenderer
+      const i = remarkPlugins.indexOf(math)
+      if (i >= 0) remarkPlugins.splice(i, 1)
+      if (remarkCodeComponents.math === Math) delete remarkCodeComponents.math
+      if (remarkCodeComponents.inline_math === Math) delete remarkCodeComponents.inline_math
+    }
+    if (CodeMirror) {
+      const { modeInfo } = CodeMirror
+      const i = modeInfo.indexOf(MATH_MODE_INFO)
+      if (i >= 0) modeInfo.splice(i, 1)
+    }
+  }
 }
