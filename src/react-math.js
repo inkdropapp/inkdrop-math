c729831b0157a76ee14c80cc8def69c0054b2716
Takuya Matsuyama <hi@craftz.dog>
2022-11-27 21:35:41 +0900

    fix(react): drop prop-types

--- src/react-math.js
+++ src/react-math.js
@@ -1,37 +1,30 @@
-'use babel'
-import * as React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 import TeX from '@matejmazur/react-katex'
 
-export default class Math extends React.Component {
-  static propTypes = {
-    lang: PropTypes.string.isRequired,
-    children: PropTypes.arrayOf(PropTypes.string)
-  }
-
-  render() {
-    const lang = this.props.lang
-    const equation = this.props.children[0]
-    if (equation) {
-      try {
-        return (
-          <TeX
-            block={lang === 'math'}
-            math={equation}
-            renderError={(error) => {
-              return (
-                <span className="ui error message mde-error-message">
-                  {error.message}
-                </span>
-              )
-            }}
-          />
-        )
-      } catch (e) {
-        return <span>{e.message}</span>
-      }
-    } else {
-      return <span>Invalid math block</span>
+const Math = props => {
+  const lang = props.lang
+  const equation = props.children[0]
+  if (equation) {
+    try {
+      return (
+        <TeX
+          block={lang === 'math'}
+          math={equation}
+          renderError={error => {
+            return (
+              <span className="ui error message mde-error-message">
+                {error.message}
+              </span>
+            )
+          }}
+        />
+      )
+    } catch (e) {
+      return <span>{e.message}</span>
     }
+  } else {
+    return <span>Invalid math block</span>
   }
 }
+
+export default Math
