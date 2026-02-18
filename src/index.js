4ca90561976b30ee51672a5d78a8e877aabadd3b
ka <k.andzhanovskii@gmail.com>
2026-02-18 08:15:41 +0000

    refactor: convert to ES module exports
    Replace module.exports with named ES exports (activate, deactivate) for
    consistency with the ES import statements at the top of the file. The
    Rollup config's exports: 'auto' handles the CJS output correctly.

--- src/index.js
+++ src/index.js
@@ -11,28 +11,26 @@ const MATH_MODE_INFO = {
 
 const ReactMath = lazy(() => import('./react-math'))
 
-module.exports = {
-  activate() {
-    if (markdownRenderer) {
-      markdownRenderer.remarkCodeComponents.math = ReactMath
-      markdownRenderer.remarkCodeComponents.inline_math = ReactMath
+export function activate() {
+  if (markdownRenderer) {
+    markdownRenderer.remarkCodeComponents.math = ReactMath
+    markdownRenderer.remarkCodeComponents.inline_math = ReactMath
+  }
+  if (CodeMirror) {
+    if (!CodeMirror.modeInfo.some(m => m.name === 'math')) {
+      CodeMirror.modeInfo.push(MATH_MODE_INFO)
     }
-    if (CodeMirror) {
-      if (!CodeMirror.modeInfo.some(m => m.name === 'math')) {
-        CodeMirror.modeInfo.push(MATH_MODE_INFO)
-      }
-    }
-  },
+  }
+}
 
-  deactivate() {
-    if (markdownRenderer) {
-      markdownRenderer.remarkCodeComponents.math = null
-      markdownRenderer.remarkCodeComponents.inline_math = null
-    }
-    if (CodeMirror) {
-      const { modeInfo } = CodeMirror
-      const i = modeInfo.findIndex(m => m.name === 'math')
-      if (i >= 0) modeInfo.splice(i, 1)
-    }
+export function deactivate() {
+  if (markdownRenderer) {
+    markdownRenderer.remarkCodeComponents.math = null
+    markdownRenderer.remarkCodeComponents.inline_math = null
+  }
+  if (CodeMirror) {
+    const { modeInfo } = CodeMirror
+    const i = modeInfo.findIndex(m => m.name === 'math')
+    if (i >= 0) modeInfo.splice(i, 1)
   }
 }
